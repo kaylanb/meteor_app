9bf713c9acfc09eb80bd4cd2c2c56486af915ebe
Kaylan Burleigh <kaylanb@berkeley.edu>
2017-08-17 22:25:35 -0700

    meteor create meteor_app --full AND d3.js plots integrated with meteor Templating

---
+++ imports/ui/pages/home/home.js
@@ -0,0 +1,6 @@
+import './home.html';
+
+import '../../components/hello/hello.js';
+import '../../components/info/info.js';
+// import '../../components/plotGeo/plotGeo.js';
+import '../../components/plotLine/plotLine.js';
