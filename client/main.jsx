bef8be72f086e757032626d52c9ba7cce808617c
Kaylan Burleigh <kaylanb@berkeley.edu>
2017-08-25 15:10:15 -0700

    succesfully moved LineChart code to file and import it, also es5 -> es6

--- client/main.jsx
+++ client/main.jsx
@@ -1,127 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 import { mount } from 'react-mounter';
 import { Meteor } from 'meteor/meteor';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Tiles } from '../imports/db.js';
 import { ZPT } from '../imports/db.js';
-
-var Dots=React.createClass({
-    propTypes: {
-        data:React.PropTypes.array,
-        xScale:React.PropTypes.func,
-        yScale:React.PropTypes.func
- 
-    },
-    render:function(){
- 
-        var _self=this;
- 
-        //remove last & first point
-        var data=this.props.data.splice(1);
-        data.pop();
- 
-        var circles=data.map(function(d,i){
- 
-            return (<circle className="dot" 
-                      r="7" 
-                      cx={_self.props.xScale(d.date)} 
-                      cy={_self.props.yScale(d.count)} 
-                      fill="#7dc7f4"
-                      stroke="#3f5175" 
-                      strokeWidth="5px" 
-                      key={i}/>);
-        });
- 
-        return(
-            <g>
-                {circles}
-            </g>
-        );
-    }
-});
-
-var LineChart=React.createClass({
-
-    propTypes: {
-        width:React.PropTypes.number,
-        height:React.PropTypes.number,
-        chartId:React.PropTypes.string
-    },
-
-    getDefaultProps: function() {
-        return {
-            width: 600,
-            height: 300,
-            chartId: 'v1_chart'
-        };
-    },
-    getInitialState:function(){
-        return {
-            width:this.props.width
-        };
-    },
-    render:function(){
-        var data=[
-            {day:'02-11-2016',count:180},
-            {day:'02-12-2016',count:250},
-            {day:'02-13-2016',count:150},
-            {day:'02-14-2016',count:496},
-            {day:'02-15-2016',count:140},
-            {day:'02-16-2016',count:380},
-            {day:'02-17-2016',count:100},
-            {day:'02-18-2016',count:150}
-        ];
-
-        var margin = {top: 5, right: 50, bottom: 20, left: 50},
-            w = this.state.width - (margin.left + margin.right),
-            h = this.props.height - (margin.top + margin.bottom);
-
-        var parseDate = d3.time.format("%m-%d-%Y").parse;
-
-        data.forEach(function (d) {
-            d.date = parseDate(d.day);
-        });
-
-        var xScale = d3.time.scale()
-            .domain(d3.extent(data, function (d) {
-                return d.date;
-            }))
-            .rangeRound([0, w]);
-
-        var yScale = d3.scale.linear()
-            .domain([0,d3.max(data,function(d){
-                return d.count+100;
-            })])
-            .range([h, 0]);
-        
-        var line = d3.svg.line()
-            .x(function (d) {
-                return xScale(d.date);
-            })
-            .y(function (d) {
-                return yScale(d.count);
-            }).interpolate('cardinal');
-
-        var transform='translate(' + margin.left + ',' + margin.top + ')';
-
-        return (
-            <div>
-                <svg id={this.props.chartId} width={this.state.width} height={this.props.height}>
-
-                    <g transform={transform}>
-                        <path className="line shadow" d={line(data)} strokeLinecap="round"/>
-                        <Dots data={data} xScale={xScale} yScale={yScale}/>
-                    </g>
-                </svg>
-            </div>
-        );
-    }
-});
+import LineChart from '../imports/LineChart.jsx';
 
 
-var Visitors = React.createClass({
-    render:function(){
+class Visitors extends React.Component {
+    render() {
         return (
             <div>
                 <h3>Visitors to your site</h3>
@@ -131,7 +20,7 @@ var Visitors = React.createClass({
             </div>
         )
     }
-});
+};
 
 // ReactDOM.render(
 //   <Visitors/>,
@@ -157,13 +46,15 @@ class Pages extends React.Component {
   // Returns the top level html to render each page
   getPlotSvg() {
     if (this.props.page == 1) {
-      var cx = 100;
+      var html = <Visitors />;
     } else if (this.props.page == 2) {
       var cx = 200;
+      var html = <BasicSvg width="400" height="400" cx={cx} />;
     } else {
       var cx = 300;
+      var html = <BasicSvg width="400" height="400" cx={cx} />;
     }
-    return ( <BasicSvg width="400" height="400" cx={cx} />  );
+    return ( html );
   }
 
   render() {
@@ -172,9 +63,8 @@ class Pages extends React.Component {
     return (
       <div className="page" id={page_id}>page {this.props.page}
         {this.getPlotSvg()}
-        <Visitors />
       </div>
-   );
+    );
   }
 }
 
