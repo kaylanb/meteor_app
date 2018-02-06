8010a2c96f8ceebcfb554dfa59d3f99f67ca1a2a
Kaylan Burleigh <kaylanb@berkeley.edu>
2018-02-06 07:41:24 -0800

    example JS code

---
+++ imports/ExampleSyntax.js
@@ -0,0 +1,66 @@
+// undefined: means variable created but value is unassigned
+// null: is a type of value, doesn't map to False
+// bool: true,false
+// Nans: math between non-numberic and numberic, let a=10/"hello"
+// Infinity: let a=1/0 is same as let a=Infinity
+// Strings
+// "a" and 'a' are the same
+// let a=`stuff ${variable + 5}` is also okay
+
+// Math stuff
+// ++,--, 12 % 10, 2**3, 
+
+// if () {
+// } elif () {
+// } else {
+// }
+
+// while () {
+// }
+
+// for (let i=0; i< 10; i++) {
+// }
+
+// function Hello(a=10,text="Hello Kaylan") {
+//   return a;   
+// }
+
+// Lambda Function
+// let func (a=10,text="Hello Kaylan") => a;
+
+// Dictionaries (aka Objects in JS)
+// let a= {b:10, k:"hello"};
+// a.c === undefined OR "k" in a
+
+// More complicated objects
+// let a ={
+//     b=10;
+//     funcPrint() {
+//         alert(this.b);
+//     }
+// }
+
+// Classes
+// class Animal {
+
+//   constructor(name) {
+//     this.speed = 0;
+//     this.name = name;
+//   }
+
+//   stop() {
+//     this.speed = 0;
+//     alert(`${this.name} stopped.`);
+//   }
+
+// }
+
+// class Rabbit extends Animal {
+
+//   stop() {
+//     super.stop(); // call parent stop
+//     alert('I am a rabbit');
+//   }
+// }
+
+ 
